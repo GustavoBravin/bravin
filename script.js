75cf0236df6a8345ce61461b9b9f0010b13ee6fa
GustavoBravin <gugarbravin@gmail.com>
2023-05-17 14:29:32 -0400

    produto destaque

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ this.preco = preco;
 }
 
 Mostrar_produtos(){
-    return `<div>${this.nome}</div> 
-    <div>${this.cadastro}</div> 
-    <div>${this.descricao}</div> 
-    <div>${this.preco}</div> `
+    return this.nome + this.cadastro + this.descricao + this.preco
+    
+
     
 }
 
@@ -22,7 +21,13 @@ super(nome,cadastro, descricao ,preco)
 this.imagem = imagem;
 }
 Mostrar_produtos_destaque(){
-    return this.nome + this.cadastro + this.descricao + this.preco + this.imagem
+    return `
+    <center><img class ="img" src= " ${this.imagem}"><img></center>
+    <div>${this.nome}</div> 
+    <div>${this.cadastro}</div> 
+    <div>${this.descricao}</div> 
+    <div>${this.preco}</div> 
+    `
 
 }
 
@@ -30,8 +35,8 @@ Mostrar_produtos_destaque(){
 let produto = new Produto("Garrafa","GBravin","carrega liquidos",30)
 console.log(produto.Mostrar_produtos())
  
-let produtodestaque = new ProdutoDestaque("Garrafa "," Bravin"," carrega liquidos ", 30," www.imagem.com")
+let produtodestaque = new ProdutoDestaque("Garrafa "," Bravin"," carrega liquidos ", 30,"https://s2.glbimg.com/ZHcY8qtdiU1f837RYKPLd7PzZmE=/166x0:563x430/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/X/A/Cq4NMnT7GuABc7PRxGbw/sem-titulo-1.jpg")
 console.log(produtodestaque.Mostrar_produtos_destaque())
 
 const div = document.getElementById("destaque")
-div.insertAdjacentHTML("afterbegin", produto.Mostrar_produtos)
+div.insertAdjacentHTML("afterbegin",produtodestaque.Mostrar_produtos_destaque() )
