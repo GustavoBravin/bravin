7c852aaf984fa3e9ed40e2141cfac4be3c084f53
Luiz F. Picolo <gugarbravin@gmail.com>
2023-05-24 14:09:49 -0400

    atualizado

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ this.imagem = imagem;
 }
 Mostrar_produtos_destaque(){
     return `
-    <center><img class ="img" src= " ${this.imagem}"><img></center>
-    <div>${this.nome}</div> 
     <div>${this.cadastro}</div> 
+    <div>${this.nome}</div> 
+    <center><img class ="img" src= " ${this.imagem}"><img></center>
     <div>${this.descricao}</div> 
     <div>${this.preco}</div> 
     `
@@ -32,11 +32,14 @@ Mostrar_produtos_destaque(){
 }
 
 }
-let produto = new Produto("Garrafa","GBravin","carrega liquidos",30)
+let produto = new Produto("Garrafa Stanley","GBravin","carrega liquidos","R$200")
 console.log(produto.Mostrar_produtos())
  
-let produtodestaque = new ProdutoDestaque("Garrafa "," Bravin"," carrega liquidos ", 30,"https://s2.glbimg.com/ZHcY8qtdiU1f837RYKPLd7PzZmE=/166x0:563x430/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/X/A/Cq4NMnT7GuABc7PRxGbw/sem-titulo-1.jpg")
+let produtodestaque = new ProdutoDestaque("Garrafa "," Bravin"," carrega liquidos ", "R$:30","https://s2.glbimg.com/ZHcY8qtdiU1f837RYKPLd7PzZmE=/166x0:563x430/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/X/A/Cq4NMnT7GuABc7PRxGbw/sem-titulo-1.jpg")
 console.log(produtodestaque.Mostrar_produtos_destaque())
 
 const div = document.getElementById("destaque")
 div.insertAdjacentHTML("afterbegin",produtodestaque.Mostrar_produtos_destaque() )
+
+const div2 = document.getElementById("lista-produtos")
+div2.insertAdjacentHTML("afterbegin",produto.Mostrar_produtos() )
